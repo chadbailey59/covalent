a3298a44260e7a3a12de739b98e8ad2c0dca98d0
Chad Bailey <chadbailey@gmail.com>
2016-06-29 16:40:41 +0000

    initial commit

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const {dialog} = require('electron')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,13 +13,16 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
+  mainWindow = new BrowserWindow({width: 1024, height: 768})
+  // if I want to make it Mavericks-style: titleBarStyle: 'hidden' above
+  // see http://electron.atom.io/docs/api/frameless-window/ for how to add a
+  // draggable region, etc
+  
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`https://hangouts.google.com`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -27,10 +33,184 @@ function createWindow () {
   })
 }
 
+function createMenu() {
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Join Hangout from URL…',
+          accelerator: 'CmdOrCtrl+N',
+          click(item, focusedWindow, event) {
+            // eventually this can smartly reuse the main window if it exists
+            newWindow = new BrowserWindow({width: 1024, height: 768})
+            newWindow.loadURL('file://' + __dirname + '/index.html')
+            newWindow.on('closed', function () { newWindow = null })
+          }
+          
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          role: 'undo'
+        },
+        {
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'cut'
+        },
+        {
+          role: 'copy'
+        },
+        {
+          role: 'paste'
+        },
+        {
+          role: 'pasteandmatchstyle'
+        },
+        {
+          role: 'delete'
+        },
+        {
+          role: 'selectall'
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click(item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload();
+          }
+        },
+        {
+          role: 'togglefullscreen'
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.webContents.toggleDevTools();
+          }
+        },
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {
+          label: 'Always On Top',
+          accelerator: 'CmdOrCtrl+T',
+          type: 'checkbox',
+          click(item, focusedWindow) {
+            if (item.checked == true) {
+              focusedWindow.setAlwaysOnTop(true);
+            } else {
+              focusedWindow.setAlwaysOnTop(false);
+            }
+          }
+        },
+        {
+          role: 'minimize'
+        },
+        {
+          role: 'close'
+        },
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() { require('electron').shell.openExternal('http://electron.atom.io'); }
+        },
+      ]
+    },
+  ];
+
+  if (process.platform === 'darwin') {
+    const name = app.getName();
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'hide'
+        },
+        {
+          role: 'hideothers'
+        },
+        {
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'quit'
+        },
+      ]
+    });
+    // Window menu.
+    template[3].submenu = [
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Zoom',
+        role: 'zoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front'
+      }
+    ];
+  }
+
+  menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+app.on('ready', createMenu)
+// Create the menu item to open a new Hangouts window.
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -49,5 +229,11 @@ app.on('activate', function () {
   }
 })
 
+app.on('open-url', function(event, url) {
+  event.preventDefault()
+  newWindow = new BrowserWindow({width: 1024, height: 768})
+  newWindow.loadURL(url)
+  newWindow.on('closed', function () { newWindow = null })
+})
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
